f8717d306a06d6f838f8f7c1f6e2d3971320b519
Clear <576839360@qq.com>
2016-05-11 19:03:30 +0800

    add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,36 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: ['./src/main.js'],
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
-  },
-  resolve: {
-    extensions: ['', '.js', '.vue', '.css']
-  },
-  module: {
-    // avoid webpack trying to shim process
-    noParse: /es6-promise\.js$/,
-    loaders: [
-      {
-        test: /\.js$/,
-        // for normal use cases only node_modules is needed.
-        loaders: ['babel'],
-        exclude: [/node_modules/]
-      },
-      {
-        test: /\.vue$/,
-        loaders: ['vue']
-      }
-    ]
-  },
-  vue: {
-    autoprefixer: false
-  },
-  babel: {
-    presets: ['es2015']
-  }
+    entry: ['./src/main.js'],
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue', '.css']
+    },
+    module: {
+        // avoid webpack trying to shim process
+        noParse: /es6-promise\.js$/,
+        loaders: [
+            {
+                test: /\.js$/,
+                // for normal use cases only node_modules is needed.
+                loaders: ['babel'],
+                exclude: [/node_modules/]
+            },
+            {
+                test: /\.vue$/,
+                loaders: ['vue']
+            }
+        ]
+    },
+    vue: {
+        autoprefixer: false
+    },
+    babel: {
+        presets: ['es2015']
+    },
+    plugins: []
 }
