84f63a165f80392c0438dd878692ebb2a3447f80
大板栗 <576839360@qq.com>
2016-05-14 19:16:00 +0800

    add loading animation

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,20 +1,25 @@
-<!-- JustClear updated at 2016-05-13 -->
+<!-- JustClear updated at 2016-05-14 -->
 
 <template>
 	<div class="loading">
-		<span class="loading-text">Loading...</span>
+        <p class="loading-text">
+            <span class="l">L</span>
+            <span class="o">o</span>
+            <span class="a">a</span>
+            <span class="d">d</span>
+            <span class="i">i</span>
+            <span class="n">n</span>
+            <span class="g">g</span>
+            <span class="dot-1">.</span>
+            <span class="dot-2">.</span>
+            <span class="dot-3">.</span>
+        </p>
 	</div>
 </template>
 
-<script>
-export default {
-    // 
-}
-</script>
-
 <style lang="sass">
 .loading {
-	position: fixed;
+	position: absolute;
     top: 0;
     left: 0;
     width: 100%;
@@ -26,6 +31,109 @@ export default {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+       -moz-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+
+    span {
+        opacity: 1;
+    }
+
+    .l {
+        -webkit-animation: transit 2s infinite ease-in-out -.7s;
+             -o-animation: transit 2s infinite ease-in-out -.7s;
+                animation: transit 2s infinite ease-in-out -.7s;
+    }
+
+    .o {
+        -webkit-animation: transit 2s infinite ease-in-out -.6s;
+             -o-animation: transit 2s infinite ease-in-out -.6s;
+                animation: transit 2s infinite ease-in-out -.6s;
+    }
+
+    .a {
+        -webkit-animation: transit 2s infinite ease-in-out -.5s;
+             -o-animation: transit 2s infinite ease-in-out -.5s;
+                animation: transit 2s infinite ease-in-out -.5s;
+    }
+
+    .d {
+        -webkit-animation: transit 2s infinite ease-in-out -.4s;
+             -o-animation: transit 2s infinite ease-in-out -.4s;
+                animation: transit 2s infinite ease-in-out -.4s;
+    }
+
+    .i {
+        -webkit-animation: transit 2s infinite ease-in-out -.3s;
+             -o-animation: transit 2s infinite ease-in-out -.3s;
+                animation: transit 2s infinite ease-in-out -.3s;
+    }
+
+    .n {
+        -webkit-animation: transit 2s infinite ease-in-out -.2s;
+             -o-animation: transit 2s infinite ease-in-out -.2s;
+                animation: transit 2s infinite ease-in-out -.2s;
+    }
+
+    .g {
+        -webkit-animation: transit 2s infinite ease-in-out -.1s;
+             -o-animation: transit 2s infinite ease-in-out -.1s;
+                animation: transit 2s infinite ease-in-out -.1s;
+    }
+
+    .dot-1 {
+        -webkit-animation: transit 2s infinite ease-in-out 0s;
+             -o-animation: transit 2s infinite ease-in-out 0s;
+                animation: transit 2s infinite ease-in-out 0s;
+    }
+
+    .dot-2 {
+        -webkit-animation: transit 2s infinite ease-in-out .1s;
+             -o-animation: transit 2s infinite ease-in-out .1s;
+                animation: transit 2s infinite ease-in-out .1s;
+    }
+
+    .dot-3 {
+        -webkit-animation: transit 2s infinite ease-in-out .2s;
+             -o-animation: transit 2s infinite ease-in-out .2s;
+                animation: transit 2s infinite ease-in-out .2s;
+    }
+
+    @-webkit-keyframes transit {
+        0% {
+            opacity: 1;
+        } 50% {
+            opacity: 0;
+        } 100% {
+            opacity: 1;
+        }
+    }
+    @-o-keyframes transit {
+        0% {
+            opacity: 1;
+        } 50% {
+            opacity: 0;
+        } 100% {
+            opacity: 1;
+        }
+    }
+    @-moz-keyframes transit {
+        0% {
+            opacity: 1;
+        } 50% {
+            opacity: 0;
+        } 100% {
+            opacity: 1;
+        }
+    }
+    @keyframes transit {
+        0% {
+            opacity: 1;
+        } 50% {
+            opacity: 0;
+        } 100% {
+            opacity: 1;
+        }
+    }
 }
 </style>
