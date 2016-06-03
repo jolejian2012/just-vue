b2772a3bc8c67d6f7a89037d2ca1d040769dcf25
Clear <576839360@qq.com>
2016-06-03 21:10:34 +0800

    add `vie-resource` dependency

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 
 import Vue from 'vue';
 import Router from 'vue-router';
+import Resource from 'vue-resource';
 import Routers from './routers';
 
 // components
 import App from './App.vue';
 
-// install router
+// install plugins
 Vue.use(Router);
+Vue.use(Resource);
 
 // instantiate routing
 let router = new Router();
